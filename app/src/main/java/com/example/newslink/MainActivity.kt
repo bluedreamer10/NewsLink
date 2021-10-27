1f4b46143e6eb8ddc7745b125290eec99cb3a9b4
Arin Verma <arinverma10@gmail.com>
2021-10-28 00:21:26 +0530

    Hello World!

---
+++ app/src/main/java/com/example/newslink/MainActivity.kt
@@ -0,0 +1,339 @@
+package com.example.newslink
+
+import android.net.Uri
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import androidx.browser.customtabs.CustomTabsIntent
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.android.volley.AuthFailureError
+import com.android.volley.Request
+import com.android.volley.Response
+import com.android.volley.toolbox.JsonObjectRequest
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity(), NewsItemClicked {
+    private lateinit var mAdapter: NewsListAdapter
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        recyclerView.layoutManager = LinearLayoutManager(this)
+        fetchDataIndia()
+        mAdapter = NewsListAdapter(this)
+        recyclerView.adapter = mAdapter
+
+        btnIndia.setOnClickListener{
+            fetchDataIndia()
+            mAdapter = NewsListAdapter(this)
+            recyclerView.adapter = mAdapter
+        }
+        btnTech.setOnClickListener{
+            fetchDataTech()
+            mAdapter = NewsListAdapter(this)
+            recyclerView.adapter = mAdapter
+        }
+        btnBusiness.setOnClickListener{
+            fetchDataBusiness()
+            mAdapter = NewsListAdapter(this)
+            recyclerView.adapter = mAdapter
+        }
+        btnEntertainment.setOnClickListener{
+            fetchDataEntertainment()
+            mAdapter = NewsListAdapter(this)
+            recyclerView.adapter = mAdapter
+        }
+        btnHealth.setOnClickListener{
+            fetchDataHealth()
+            mAdapter = NewsListAdapter(this)
+            recyclerView.adapter = mAdapter
+        }
+        btnScience.setOnClickListener{
+            fetchDataScience()
+            mAdapter = NewsListAdapter(this)
+            recyclerView.adapter = mAdapter
+        }
+        btnSports.setOnClickListener{
+            fetchDataSports()
+            mAdapter = NewsListAdapter(this)
+            recyclerView.adapter = mAdapter
+        }
+
+    }
+
+    private fun fetchDataIndia() {
+
+        var url = "https://newsapi.org/v2/top-headlines?country=in&apiKey=c92e05934e5c48a1a1878076909b601a"
+
+
+        val jsonObjectRequest = object :JsonObjectRequest(Request.Method.GET, url, null,
+
+            { response ->
+                val newsJsonArray = response.getJSONArray("articles")
+                val newsArray = ArrayList<News>()
+                for (i in 0 until newsJsonArray.length()) {
+                    val newsJsonObject = newsJsonArray.getJSONObject(i)
+                    val news = News(
+                        newsJsonObject.getString("title"),
+                        newsJsonObject.getString("author"),
+                        newsJsonObject.getString("url"),
+                        newsJsonObject.getString("urlToImage")
+                    )
+                    newsArray.add(news)
+                }
+
+                mAdapter.updateNews(newsArray)
+
+            },
+            { _ ->
+
+            })
+
+        {
+            @Throws(AuthFailureError::class)
+            override fun getHeaders(): Map<String, String>? {
+                val headers = HashMap<String, String>()
+                headers["User-Agent"] = "Mozilla/5.0"
+                return headers
+            }
+        }
+        // Access the RequestQueue through your singleton class.
+        MySingleton.getInstance(this).addToRequestQueue(jsonObjectRequest)
+    }
+    private fun fetchDataTech() {
+
+        var url = "https://newsapi.org/v2/top-headlines?country=in&category=technology&apiKey=c92e05934e5c48a1a1878076909b601a"
+
+
+        val jsonObjectRequest = object :JsonObjectRequest(Request.Method.GET, url, null,
+
+            { response ->
+                val newsJsonArray = response.getJSONArray("articles")
+                val newsArray = ArrayList<News>()
+                for (i in 0 until newsJsonArray.length()) {
+                    val newsJsonObject = newsJsonArray.getJSONObject(i)
+                    val news = News(
+                        newsJsonObject.getString("title"),
+                        newsJsonObject.getString("author"),
+                        newsJsonObject.getString("url"),
+                        newsJsonObject.getString("urlToImage")
+                    )
+                    newsArray.add(news)
+                }
+
+                mAdapter.updateNews(newsArray)
+
+            },
+            { _ ->
+
+            })
+
+        {
+            @Throws(AuthFailureError::class)
+            override fun getHeaders(): Map<String, String>? {
+                val headers = HashMap<String, String>()
+                headers["User-Agent"] = "Mozilla/5.0"
+                return headers
+            }
+        }
+        // Access the RequestQueue through your singleton class.
+        MySingleton.getInstance(this).addToRequestQueue(jsonObjectRequest)
+    }
+    private fun fetchDataBusiness(){
+        var url = "https://newsapi.org/v2/top-headlines?country=in&category=business&apiKey=c92e05934e5c48a1a1878076909b601a"
+
+
+        val jsonObjectRequest = object :JsonObjectRequest(Request.Method.GET, url, null,
+
+            { response ->
+                val newsJsonArray = response.getJSONArray("articles")
+                val newsArray = ArrayList<News>()
+                for (i in 0 until newsJsonArray.length()) {
+                    val newsJsonObject = newsJsonArray.getJSONObject(i)
+                    val news = News(
+                        newsJsonObject.getString("title"),
+                        newsJsonObject.getString("author"),
+                        newsJsonObject.getString("url"),
+                        newsJsonObject.getString("urlToImage")
+                    )
+                    newsArray.add(news)
+                }
+
+                mAdapter.updateNews(newsArray)
+
+            },
+            { _ ->
+
+            })
+
+        {
+            @Throws(AuthFailureError::class)
+            override fun getHeaders(): Map<String, String>? {
+                val headers = HashMap<String, String>()
+                headers["User-Agent"] = "Mozilla/5.0"
+                return headers
+            }
+        }
+        // Access the RequestQueue through your singleton class.
+        MySingleton.getInstance(this).addToRequestQueue(jsonObjectRequest)
+    }
+    private fun fetchDataEntertainment(){
+        var url = "https://newsapi.org/v2/top-headlines?country=in&category=entertainment&apiKey=c92e05934e5c48a1a1878076909b601a"
+
+
+        val jsonObjectRequest = object :JsonObjectRequest(Request.Method.GET, url, null,
+
+            { response ->
+                val newsJsonArray = response.getJSONArray("articles")
+                val newsArray = ArrayList<News>()
+                for (i in 0 until newsJsonArray.length()) {
+                    val newsJsonObject = newsJsonArray.getJSONObject(i)
+                    val news = News(
+                        newsJsonObject.getString("title"),
+                        newsJsonObject.getString("author"),
+                        newsJsonObject.getString("url"),
+                        newsJsonObject.getString("urlToImage")
+                    )
+                    newsArray.add(news)
+                }
+
+                mAdapter.updateNews(newsArray)
+
+            },
+            { _ ->
+
+            })
+
+        {
+            @Throws(AuthFailureError::class)
+            override fun getHeaders(): Map<String, String>? {
+                val headers = HashMap<String, String>()
+                headers["User-Agent"] = "Mozilla/5.0"
+                return headers
+            }
+        }
+        // Access the RequestQueue through your singleton class.
+        MySingleton.getInstance(this).addToRequestQueue(jsonObjectRequest)
+    }
+    private fun fetchDataHealth(){
+        var url = "https://newsapi.org/v2/top-headlines?country=in&category=health&apiKey=c92e05934e5c48a1a1878076909b601a"
+
+
+        val jsonObjectRequest = object :JsonObjectRequest(Request.Method.GET, url, null,
+
+            { response ->
+                val newsJsonArray = response.getJSONArray("articles")
+                val newsArray = ArrayList<News>()
+                for (i in 0 until newsJsonArray.length()) {
+                    val newsJsonObject = newsJsonArray.getJSONObject(i)
+                    val news = News(
+                        newsJsonObject.getString("title"),
+                        newsJsonObject.getString("author"),
+                        newsJsonObject.getString("url"),
+                        newsJsonObject.getString("urlToImage")
+                    )
+                    newsArray.add(news)
+                }
+
+                mAdapter.updateNews(newsArray)
+
+            },
+            { _ ->
+
+            })
+
+        {
+            @Throws(AuthFailureError::class)
+            override fun getHeaders(): Map<String, String>? {
+                val headers = HashMap<String, String>()
+                headers["User-Agent"] = "Mozilla/5.0"
+                return headers
+            }
+        }
+        // Access the RequestQueue through your singleton class.
+        MySingleton.getInstance(this).addToRequestQueue(jsonObjectRequest)
+    }
+    private fun fetchDataScience(){
+        var url = "https://newsapi.org/v2/top-headlines?country=in&category=science&apiKey=c92e05934e5c48a1a1878076909b601a"
+
+
+        val jsonObjectRequest = object :JsonObjectRequest(Request.Method.GET, url, null,
+
+            { response ->
+                val newsJsonArray = response.getJSONArray("articles")
+                val newsArray = ArrayList<News>()
+                for (i in 0 until newsJsonArray.length()) {
+                    val newsJsonObject = newsJsonArray.getJSONObject(i)
+                    val news = News(
+                        newsJsonObject.getString("title"),
+                        newsJsonObject.getString("author"),
+                        newsJsonObject.getString("url"),
+                        newsJsonObject.getString("urlToImage")
+                    )
+                    newsArray.add(news)
+                }
+
+                mAdapter.updateNews(newsArray)
+
+            },
+            { _ ->
+
+            })
+
+        {
+            @Throws(AuthFailureError::class)
+            override fun getHeaders(): Map<String, String>? {
+                val headers = HashMap<String, String>()
+                headers["User-Agent"] = "Mozilla/5.0"
+                return headers
+            }
+        }
+        // Access the RequestQueue through your singleton class.
+        MySingleton.getInstance(this).addToRequestQueue(jsonObjectRequest)
+    }
+    private fun fetchDataSports(){
+        var url = "https://newsapi.org/v2/top-headlines?country=in&category=sports&apiKey=c92e05934e5c48a1a1878076909b601a"
+
+
+        val jsonObjectRequest = object :JsonObjectRequest(Request.Method.GET, url, null,
+
+            { response ->
+                val newsJsonArray = response.getJSONArray("articles")
+                val newsArray = ArrayList<News>()
+                for (i in 0 until newsJsonArray.length()) {
+                    val newsJsonObject = newsJsonArray.getJSONObject(i)
+                    val news = News(
+                        newsJsonObject.getString("title"),
+                        newsJsonObject.getString("author"),
+                        newsJsonObject.getString("url"),
+                        newsJsonObject.getString("urlToImage")
+                    )
+                    newsArray.add(news)
+                }
+
+                mAdapter.updateNews(newsArray)
+
+            },
+            { _ ->
+
+            })
+
+        {
+            @Throws(AuthFailureError::class)
+            override fun getHeaders(): Map<String, String>? {
+                val headers = HashMap<String, String>()
+                headers["User-Agent"] = "Mozilla/5.0"
+                return headers
+            }
+        }
+        // Access the RequestQueue through your singleton class.
+        MySingleton.getInstance(this).addToRequestQueue(jsonObjectRequest)
+    }
+
+
+    override fun onItemClicked(items: News) {
+        val builder = CustomTabsIntent.Builder()
+        val customTabsIntent = builder.build()
+        customTabsIntent.launchUrl(this, Uri.parse(items.url))
+    }
+}
